0ed5f3f8df9a092885b27ff5038cbc6aacde7449
Venkata Suresh M FSA <52411849+venkat-fsa@users.noreply.github.com>
2021-09-26 12:48:37 +0100

    comments added

--- src/model.py
+++ src/model.py
@@ -1,3 +1,5 @@
+
+
 from io import BytesIO
 import numpy as np
 import tensorflow as tf
@@ -7,16 +9,23 @@ from tensorflow.keras.applications import imagenet_utils
 
 model = None
 
+
+# Load the pre-trained Keras model (here we are using a model
+# pre-trained on ImageNet and provided by Keras, but you can
+# substitute in your own networks just as easily)
+
 class imageClassifier:
 
     def __init__(self):
         self.model = tf.keras.applications.densenet.DenseNet121(weights="imagenet")
         print("Model loaded")
-
+    
+    # read image
     def read_image(self, image_encoded):
         pil_image = Image.open(BytesIO(image_encoded))
         return pil_image
 
+    #image pre-processing
     def preprocess(self, image: Image.Image):
         image = image.resize((224, 224))
         image = np.asarray(image)[..., :3]
@@ -24,6 +33,7 @@ class imageClassifier:
         image = image / 127.5 - 1.0
         return image
 
+    #final prediction
     def predict(self, image : np.ndarray):
         prediction = self.model.predict(image)
         prediction = imagenet_utils.decode_predictions(prediction)[0][0][1]
